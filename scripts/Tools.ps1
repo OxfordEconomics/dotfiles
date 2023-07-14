53e3fceae7bd917f7ee93ef73c5af385bd5d15f5
Steven Dawkins <sdawkins@oxfordeconomics.com>
2023-07-14 22:17:17 +0100

    customized tools

--- scripts/Tools.ps1
+++ scripts/Tools.ps1
@@ -12,23 +12,23 @@ choco install -y paint.net
 choco install -y screentogif
 choco install -y zoomit
 winget install -e -h --id WinDirStat.WinDirStat
-winget install -e -h --id Microsoft.BingWallpaper
-winget install -e -h --id Obsidian.Obsidian
-winget install -e -h --id JohnMacFarlane.Pandoc
-winget install -e -h --id Microsoft.Whiteboard -s msstore
-winget install -e -h --id Microsoft.PowerToys # settings to sync
+#winget install -e -h --id Microsoft.BingWallpaper
+#winget install -e -h --id Obsidian.Obsidian
+#winget install -e -h --id JohnMacFarlane.Pandoc
+#winget install -e -h --id Microsoft.Whiteboard -s msstore
+#winget install -e -h --id Microsoft.PowerToys # settings to sync
 # Already  installed by default
 # winget install -e -h --id Microsoft.Teams
 # winget install -e -h --id Microsoft.Office
-winget install -e -h --id Logitech.Options
-winget install -e -h --id Dell.DisplayManager
+#winget install -e -h --id Logitech.Options
+#winget install -e -h --id Dell.DisplayManager
 
 # ---------------------------------------------- #
 # Dev tools  ----------------------------------- #
 # ---------------------------------------------- #
-winget install -e -h --id AndreasWascher.RepoZ
+#winget install -e -h --id AndreasWascher.RepoZ
 winget install -e -h --id CoreyButler.NVMforWindows
-iwr https://get.pnpm.io/install.ps1 -useb | iex
+#iwr https://get.pnpm.io/install.ps1 -useb | iex
 # iwr -useb get.scoop.sh | iex
 
 
@@ -60,17 +60,9 @@ New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Roaming\nushell\
 # Windows Terminal ----------------------------- #
 # ---------------------------------------------- #
 # Windows Terminal (stable + preview) install with Cascadia Code PL font
-winget install -e -h --id Microsoft.WindowsTerminal -s msstore
-winget install -e -h --id Microsoft.WindowsTerminalPreview -s msstore
-choco install -y cascadiacodepl
-# Windows terminal configuration
-Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Force
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\dotfiles\config\windowsTerminal\settings.json"
-cp "$env:USERPROFILE\dotfiles\config\windowsTerminal\icons\*" "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\RoamingState\"
-# Windows terminal preview configuration
-Remove-Item -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Force
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json" -Target "$env:USERPROFILE\dotfiles\config\windowsTerminal\settings.json"
-cp "$env:USERPROFILE\dotfiles\config\windowsTerminal\icons\*" "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\RoamingState\"
+#winget install -e -h --id Microsoft.WindowsTerminal -s msstore
+#winget install -e -h --id Microsoft.WindowsTerminalPreview -s msstore
+#choco install -y cascadiacodepl
 
 # ---------------------------------------------- #
 # Azure tools  --------------------------------- #
@@ -80,7 +72,7 @@ winget install -e -h --id Microsoft.AzureCosmosEmulator
 winget install -e -h --id Microsoft.AzureDataStudio
 winget install -e -h --id Microsoft.azure-iot-explorer
 winget install -e -h --id Microsoft.AzureStorageExplorer
-winget install -e -h --id Pulumi.Pulumi
+#winget install -e -h --id Pulumi.Pulumi
 winget install -e -h --id Microsoft.AzureFunctionsCoreTools
 # Azurite can be installed through vscode extension or as a global npm package
 # pnpm add -g azurite
